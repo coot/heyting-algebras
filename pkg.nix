9b3c5290ed1ad1f12f022231b99651c8fe8281bb
Marcin Szamotulski <profunctor@pm.me>
2018-10-10 11:07:55 +0200

    BoolRing added

--- pkg.nix
+++ pkg.nix
@@ -1,10 +1,10 @@
-{ mkDerivation, base, free-algebras, lattices, QuickCheck, tagged, tasty, tasty-quickcheck, nixpkgs, stdenv }:
+{ mkDerivation, base, free-algebras, lattices, QuickCheck, semiring-simple, tagged, tasty, tasty-quickcheck, nixpkgs, stdenv }:
 mkDerivation {
   pname = "heyting-algebras";
   version = "0.0.1.2";
   src = nixpkgs.lib.sourceFilesBySuffices ./.
     [ ".hs" "LICENSE" "ChangeLog.md" "heyting-algebras.cabal" ];
-  libraryHaskellDepends = [ base free-algebras lattices QuickCheck tagged ];
+  libraryHaskellDepends = [ base free-algebras lattices QuickCheck semiring-simple tagged ];
   testHaskellDepends = [ base lattices QuickCheck tasty tasty-quickcheck ];
   license = stdenv.lib.licenses.mpl20;
   enableSeparateDocOutput = false;
