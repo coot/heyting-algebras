dad4a2615273d1db24ee851001bc4e527ebaaea6
Marcin Szamotulski <profunctor@pm.me>
2018-12-13 00:59:20 +0100

    Updated nix environment

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,10 +1,8 @@
-{ compiler ? "ghc843" }:
+{}:
 with builtins;
 let
-  rev = "6a3f5bcb061e1822f50e299f5616a0731636e4e7";
+  rev = "722fcbbb80b2142583e9266efe77992f8e32ac4c";
   url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-  # nix-prefetch-url --unpack
-  sha256 = "1ib96has10v5nr6bzf7v8kw7yzww8zanxgw2qi1ll1sbv6kj6zpd";
   config =
     { packageOverrides = super:
       let self = super.pkgs;
@@ -12,19 +10,9 @@ let
       in {
         haskell = super.haskell // {
           packages = super.haskell.packages // {
-            ghc861 = super.haskell.packages.ghc861.override {
+            ghc862 = super.haskell.packages.ghc861.override {
               overrides = self: super: {
-                free-algebras = super.callPackage ./free-algebras-0.0.5.0.nix {};
-              };
-            };
-            ghc843 = super.haskell.packages.ghc843.override {
-              overrides = self: super: {
-                free-algebras = super.callPackage ./free-algebras-0.0.5.0.nix {};
-              };
-            };
-            ghc822 = super.haskell.packages.ghc822.override {
-              overrides = self: super: {
-                free-algebras = super.callPackage ./free-algebras-0.0.5.0.nix {};
+                hoopl_3_10_2_2 = self.callPackage ./hoopl-3.10.2.2.nix {};
               };
             };
             ghc802 = super.haskell.packages.ghc802.override {
@@ -34,6 +22,7 @@ let
                 async = super.callPackage ./async-2.1.1.1.nix {};
                 lifted-async = super.callPackage ./lifted-async-0.9.3.3.nix {};
                 exceptions = super.callPackage ./exceptions-0.8.3.nix {};
+                stm = super.callPackage ./stm-2.4.5.1.nix {};
                 free-algebras = super.callPackage ./free-algebras-0.0.5.0.nix {};
               };
             };
@@ -41,5 +30,5 @@ let
         };
       };
     };
-  nixpkgs = import (fetchTarball { inherit url sha256; }) { inherit config; };
+  nixpkgs = import (fetchTarball { inherit url; }) { inherit config; };
 in nixpkgs
