27b3b38e70b329097de7afa72d2c6d8b1fac3c60
Marcin Szamotulski <profunctor@pm.me>
2019-02-14 01:41:36 +0100

    Updates nix

--- default.nix
+++ default.nix
@@ -2,32 +2,28 @@
 , haddock ? true
 , test ? true
 , benchmarks ? false
-, export-properties ? true
 }:
 with builtins;
 let
   nixpkgs = import ./nix/nixpkgs.nix { inherit compiler; };
   lib = nixpkgs.haskell.lib;
   callPackage = nixpkgs.haskell.packages.${compiler}.callPackage;
-
-  export-properties' = if test then true else export-properties;
+  callCabal2nix = nixpkgs.haskell.packages.${compiler}.callCabal2nix;
 
   doHaddock = if haddock
     then lib.doHaddock
     else lib.dontHaddock;
-  doTest = if test && export-properties
+  doTest = if test
     then lib.doCheck
     else lib.dontCheck;
   doBench = if benchmarks
     then lib.doBenchmark
     else nixpkgs.lib.id;
-  doExportProperties = pkg: if export-properties'
-    then lib.enableCabalFlag pkg "export-properties"
-    else pkg;
 
-  heyting-algebras = doExportProperties(doHaddock(doTest(doBench(
-    callPackage ./pkg.nix {
-      inherit nixpkgs;
-    }))));
+  heyting-algebras = doHaddock(doTest(doBench(
+    lib.overrideCabal
+      (callCabal2nix "heyting-algebras" (nixpkgs.lib.sourceFilesBySuffices ./. [ ".hs" "LICENSE" "ChangeLog.md" "heyting-algebras.cabal"]) {})
+      (drv: { enableSeparateDocOutput = false; })
+    )));
 
 in { inherit heyting-algebras; }
